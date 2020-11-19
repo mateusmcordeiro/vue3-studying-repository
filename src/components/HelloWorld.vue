5b47d9512df31cdefaa582d00eae625d9703c27f
Mateus Cordeiro <mateusmcordeiro@hotmail.com>
2020-11-18 21:26:56 -0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <p>{{count}}</p>
+    <button @click="increase">click-me</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: {
+      type: String,
+      required: false,
+    },
+  },
+  setup() {
+    const count = ref(0)
+    const increase = () => {
+      count.value++
+    }
+
+    return {
+      count,
+      increase,
+    }
+  }
+})
+
+</script>
